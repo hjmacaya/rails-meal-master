e66e11be09b810287c2b69cd7dc9579dd1a68a93
Hernan Macaya <hjmacaya@uc.cl>
2024-02-13 14:08:48 -0300

    add: show view & calculate indicators

--- app/models/in_body.rb
+++ app/models/in_body.rb
@@ -1,14 +1,26 @@
 class InBody < ApplicationRecord
+  # Callbacks
+  before_save :calculate_indicators
+
   # Associations
   belongs_to :user
   belongs_to :nutritionist
 
   # Validations
   validates :user_id, presence: true
-  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0}
-  validates :height, presence: true, numericality: { greater_than: 0 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :height, presence: true, numericality: { greater_than: 0, less_than: 3 }
   validates :weight, presence: true, numericality: { greater_than: 0 }
   validates :genre, presence: true
   validates :date, presence: true
   validates :body_water, :protein_mass, :minerals_mass, :body_fat, :lean_mass, :mass_free_of_fat, :muscle_mass, presence: true, numericality: { greater_than: 0 }
+
+  private
+
+  # Method to set the indicators of the in_body
+  def calculate_indicators
+    self.imc = (weight / (height * height)).round(2)
+    self.percentage_body_fat = (body_fat / weight * 100).round(2)
+    self.aec = (weight - mass_free_of_fat).round(2)
+  end
 end
